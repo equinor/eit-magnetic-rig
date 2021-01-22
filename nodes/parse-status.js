7f09514260b93b4a9057a6264f3fb0263e8b0568
Ole Martin Gjersvik <olgj@equinor.com>
2021-01-22 13:49:59 +0100

    Parser now creates json objects from info results.

--- nodes/parse-status.js
+++ nodes/parse-status.js
@@ -3,8 +3,26 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg, send, done) {
+            let reg = /<(.*)>/g;
+            for (const match of msg.payload.matchAll(reg))  {
+                let body = match[1].split("|");
+                let msg = {payload: {}};
+                let status = body.shift();
+                msg.payload.status = status;
+                for (const item of body) {
+                    let pair = item.split(":");
+                    msg.payload[pair[0]] = pair[1].split(",").map(v => {
+                        const num = Number(v);
+                        if(isNaN(num)) {
+                            return v;
+                        }else {
+                            return num;
+                        }
+                    });
+                }
+                send(msg)
+            }
 
-            send([null, msg]);
             done();
         });
     }
