9e495ed5411e32d5df77d5077873f2d1a9effa37
Ole Martin Gjersvik <olgj@equinor.com>
2021-02-03 14:34:02 +0100

    Lab test

--- nodes/test-pri.js
+++ nodes/test-pri.js
@@ -3,27 +3,8 @@ const homing = require('homing');
 
 const crc16 = homing.crc16;
 
-function test() {
-    var buffer = new ArrayBuffer(9);
-        let view = new DataView(buffer);
-        view.setUint8(0, 0x02); // Start of message
-        view.setUint8(1, 0x00); // Address
-        view.setUint8(2, 0x48); // Command identification
-        view.setUint8(3, 0x53);
-        view.setUint8(4, 0x02); // Data Length
-        view.setUint8(5, 0x01);
-        view.setUint8(6, 0x02);
-        console.log(crc16(new Uint8Array(buffer.slice(0,7))));
-
-        view.setUint16(7, crc16(new Uint8Array(buffer.slice(0,7))));
-
-        console.log(buffer);
-}
-
-test();
-
-function main() {
-    let socket = net.createConnection(5000, "192.168.1.253")
+function send(ip, message) {
+    let socket = net.createConnection(5000, ip)
     socket.on("close", (hasError) => {
         console.log("close hasError:", hasError);
     });
@@ -31,24 +12,9 @@ function main() {
     socket.on("connect", () => {
         console.log("connected");
 
-        var buffer = new ArrayBuffer(9);
-        let view = new DataView(buffer);
-        view.setUint8(0, 0x02); // Start of message
-        view.setUint8(1, 0x00); // Address
-        view.setUint8(2, 0x48); // Command identification
-        view.setUint8(3, 0x53);
-        view.setUint8(4, 0x02); // Data Length
-        view.setUint8(5, 0x01);
-        view.setUint8(6, 0x02);
-        console.log(crc16(buffer.slice(0,7)));
-
-        let buff2 = buffer.slice(0,7);
-        new Uint8Array(buff2).reverse();
-        view.setUint16(7, polycrc.crc16(buffer.slice(0,7)));
-
-        console.log(buffer);
-
-        //console.log(socket.write(new Uint8Array(buffer)));
+        console.log(socket.write(new Uint8Array(message), function(e) {
+            console.log("sent", e)
+        }));
     });
 
     socket.on("data", (data) => {
@@ -76,4 +42,63 @@ function main() {
     });
 }
 
+function primaryTest() {
+    var buffer = new ArrayBuffer(8);
+    let view = new DataView(buffer);
+    view.setUint8(0, 0x02); // Start of message
+    view.setUint8(1, 0x00); // Address
+    view.setUint8(2, 'H'.charCodeAt(0)); // Command identification
+    view.setUint8(3, 'P'.charCodeAt(0));
+    view.setUint8(4, 0x01); // Data Length
+    view.setUint8(5, 0x01);
+    console.log(buffer.slice(0,6));
+    console.log(crc16(new Uint8Array(buffer.slice(0,6))));
 
+    view.setUint16(6, crc16(new Uint8Array(buffer.slice(0,6))));
+
+    console.log(buffer);
+
+    send("192.168.1.253", buffer);
+}
+
+function primaryTest2() {
+    var buffer = new ArrayBuffer(7);
+    let view = new DataView(buffer);
+    view.setUint8(0, 0x02); // Start of message
+    view.setUint8(1, 0x00); // Address
+    view.setUint8(2, 0x52); // Command identification
+    view.setUint8(3, 0x49);
+    view.setUint8(4, 0x00); // Data Length
+    console.log(buffer.slice(0,5));
+    console.log(crc16(new Uint8Array(buffer.slice(0,5))));
+
+    view.setUint16(5, crc16(new Uint8Array(buffer.slice(0,5))));
+
+    console.log(buffer);
+
+    send("192.168.1.253", buffer);
+}
+
+function secondaryTest() {
+    var buffer = new ArrayBuffer(9);
+    let view = new DataView(buffer);
+    view.setUint8(0, 0x02); // Start of message
+    view.setUint8(1, 0x00); // Address
+    view.setUint8(2, 'H'.charCodeAt(0)); // Command identification
+    view.setUint8(3, 'S'.charCodeAt(0));
+    view.setUint8(4, 0x02); // Data Length
+    view.setUint8(5, 0x01);
+    view.setUint8(6, 0x02);
+    console.log(buffer.slice(0,7));
+    console.log(crc16(new Uint8Array(buffer.slice(0,7))));
+
+    view.setUint16(7, crc16(new Uint8Array(buffer.slice(0,7))));
+
+    console.log(buffer);
+
+    send("192.168.2.61", buffer);
+}
+
+//primaryTest2();
+//data <Buffer 02 00 68 73 09 01 03 18 01 de 01 48 00 0d 5f 80 00>
+secondaryTest();
