004bf8ee9a853522c898c6b39ab459e678c32523
Jonas Peter Sørensen <jonas.peter.sorensen@gmail.com>
2025-05-09 11:46:06 +0200

    Remove outdated dep

--- homing/src/lib.rs
+++ homing/src/lib.rs
@@ -2,11 +2,6 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn crc16(msg: &[u8]) -> u16
